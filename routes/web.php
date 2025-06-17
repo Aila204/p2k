38ac8dcbee540db6b6bed2ead3788a8c021e921c
Aila204 <ailasuhailanurul0410@gmail.com>
2025-06-17 09:55:17 +0700

    update login route to use POST method

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {return view('welcome');});
-Route::get('/login', function () {return view('login');})->name('login');
+Route::post('/login', function () {return view('login');})->name('login');
