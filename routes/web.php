499b3ad6f0826ae9013cfef84ebbd54f193f478c
mutiarhma15@gmail.com <mutiarhma15@gmail.com>
2025-06-20 06:21:37 +0700

    update pengguna

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PenggunaController;
 
 Route::get('/', function () {return view('welcome');});
 Route::get('/login', function () {return view('login');})->name('login');
@@ -11,3 +12,5 @@ Route::get('/welcome', function () {return view('welcome');})->name('welcome');
 Route::post('/welcome', function () {return view('welcome');})->name('welcome');
 Route::get('/dasbord', function () {return view('dasbord');})->name('dasbord');
 Route::post('/dasbord', function () {return view('dasbord');})->name('dasbord');
+Route::get('/pengguna', [PenggunaController::class, 'index'])->name('pengguna');
+Route::post('/pengguna', [PenggunaController::class, 'index'])->name('pengguna');
